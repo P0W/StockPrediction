293b8e3a6952d82ebd891558101590174ab4b22d
P0W <powprashant@gmail.com>
2019-10-03 17:21:14 +0530

    Now plotting using d3 axes, commented out canvas drawing.

--- build/stockData/chart.js
+++ build/stockData/chart.js
@@ -1,21 +1,23 @@
 
 // set the dimensions and margins of the graph
 var margin = { top: 20, right: 20, bottom: 70, left: 50 },
-    width = 1580 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+    width = 1570 - margin.left - margin.right,
+    height = 690 - margin.top - margin.bottom;
 
 // parse the date / time
 var parseTime = d3.timeParse("%Y-%d-%d");
 
 // set the ranges
-var x = d3.scaleTime().range([0, width]).nice();
+var x = d3.scaleLinear().range([0, width]).nice();
 var y = d3.scaleLinear().range([height, 0]).nice();
+
+var xTime = d3.scaleTime().range([0, width]).nice();
 
 // define the line
 var valueline = d3.line()
-    .x(function (d) { return x(d.date); })
+    .x(function (d) { return x(d.index); })
     .y(function (d) { return y(d.price); })
-//.curve(d3.curveBasis);
+    .curve(d3.curveLinear)
 
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
@@ -29,41 +31,59 @@ var svg = d3.select("body").append("svg")
 
 
 // Get the data
-function plotStockPrice(csvName) {
+
+function setUpPlot(data) {
+
+    // Scale the range of the data
+    xTime.domain(d3.extent(data, function (d) { return d.time; }));
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xTime).ticks(20))
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-65)");
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "axis")
+        .call(d3.axisLeft(y).ticks(10));
+
+}
+
+function plotStockPrice(csvName, isPredicted) {
     d3.csv(csvName, function (error, data) {
         if (error) throw error;
 
         // format the data
-        data.forEach(function (d) {
-            d.date = parseTime(d.date);
-            d.price = +d.price;
+        data.forEach(function (d, index) {
+            d.time = parseTime(d.date),
+                d.price = +d.price,
+                d.index = index
         });
+        if (typeof isPredicted !== 'undefined' && isPredicted) {
+            // Add the valueline for Predicted Stock Prices path.
+            svg.append("path")
+                .data([data])
+                .attr("class", "line")
+                .style("stroke", "red")
+                .attr("d", valueline);
+        } else {
+            // Add the valueline for Actual Stock prices path.
+            y.domain([0.0, d3.max(data, function (d) { return d.price; })]);
+            setUpPlot(data);
+            x.domain([1.0, data.length]);
 
-        // Scale the range of the data
-        x.domain(d3.extent(data, function (d) { return d.date; }));
-        y.domain([0.0, d3.max(data, function (d) { return d.price; })]);
+            svg.append("path")
+                .data([data])
+                .attr("class", "line")
+                .style("stroke", "blue")
+                .attr("d", valueline);
+        }
 
-        // Add the valueline path.
-        svg.append("path")
-            .data([data])
-            .attr("class", "line")
-            .attr("d", valueline);
-
-        // Add the X Axis
-        svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x).ticks(10))
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("transform", "rotate(-65)");
-
-        // Add the Y Axis
-        svg.append("g")
-            .attr("class", "axis")
-            .call(d3.axisLeft(y).ticks(10));
 
     });
 }
