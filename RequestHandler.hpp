67daaec48b832a0657a5de8c5d2a67e7bd7edcdb
P0W <powprashant@gmail.com>
2019-10-16 16:28:25 +0530

    Added sample httpserver copied from boost/beast example.

---
+++ RequestHandler.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/dispatch.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/config.hpp>
+#include <algorithm>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+class RequestHandler
+{
+public:
+    RequestHandler();
+    virtual ~RequestHandler();
+
+    void startService();
+};
+
